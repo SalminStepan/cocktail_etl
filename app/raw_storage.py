802c1ca509a5cd22e91dfd21fd24ee87fb848f2a
Stepan Salmin <salmin.stepan.dev@gmail.com>
2026-07-04 15:55:51 +0500

    add loading data into raw storage

--- app/raw_storage.py
+++ app/raw_storage.py
@@ -1,7 +1,6 @@
 import json
 
 
-
 def save_raw_recipes(raw_recipes: list[dict], path: str = "data/raw_data.json") -> None:
     if not isinstance(raw_recipes, list):
         raise TypeError("raw_recipes must be a list")
@@ -10,3 +9,14 @@ def save_raw_recipes(raw_recipes: list[dict], path: str = "data/raw_data.json") 
             raise TypeError("each raw_recipe must be a dict")
     with open(path, "w", encoding="utf-8") as f:
         json.dump(raw_recipes, f, ensure_ascii=False, indent=2)
+
+def load_raw_recipes(path: str = "data/raw_data.json") -> list[dict]:
+    with open(path, "r", encoding="utf-8") as f:
+        raw_recipes = json.load(f)
+        if not isinstance(raw_recipes, list):
+            raise ValueError("raw data file must contain a list")
+        for raw_recipe in raw_recipes:
+            if not isinstance(raw_recipe, dict):
+                raise ValueError("raw recipe must be a dict")
+        
+    return raw_recipes
